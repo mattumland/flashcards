63a69822e18d53356113c61d4557bac7d110ce0a
Matt Umland <mattumland@gmail.com>
2021-02-01 20:46:53 -0500

    Add complete Turn.js and Turn-test.js

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,50 +2,50 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
-const Card = require('../scr/Card');
+const Card = require('../src/Card');
 
 describe('Card', function() {
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     const turn = new Turn();
 
     expect(Turn).to.be.a('function');
   });
 
-  it.skip('should be an instance of Turn', function() {
+  it('should be an instance of Turn', function() {
     const turn = new Turn();
 
     expect(turn).to.be.an.instanceof(Turn);
   });
 
-  it.skip('should be able to have a guess', function() {
+  it('should be able to have a guess', function() {
     const turn = new Turn('Flaming dog poo');
 
     expect(turn.guess).to.equal('Flaming dog poo');
   });
 
-  it.skip('should start with a card', function() {
+  it('should start with a card', function() {
     const card = new Card(1,'What is your favorite food and why is it pizza?',['duh','come on','double duh'],'double duh');
     const turn = new Turn('duh', card);
 
     expect(turn.currentCard).to.equal(card);
   });
 
-  it.skip('should be able to return the guess', function() {
+  it('should be able to return the guess', function() {
     const card = new Card(1,'What is your favorite food and why is it pizza?',['duh','come on','double duh'],'double duh');
     const turn = new Turn('duh', card);
 
     expect(turn.returnGuess()).to.equal('duh');
   });
 
-  it.skip('should be able to return the card', function() {
+  it('should be able to return the card', function() {
     const card = new Card(1,'What is your favorite food and why is it pizza?',['duh','come on','double duh'],'double duh');
     const turn = new Turn('duh', card);
 
     expect(turn.returnCard()).to.equal(card);
   });
 
-  it.skip('should be able to determine if the guess is correct', function() {
+  it('should be able to determine if the guess is correct', function() {
     const cardA = new Card(1,'What is your favorite food and why is it pizza?',['duh','come on','double duh'],'double duh');
     const turnA = new Turn('duh', cardA);
     const cardB = new Card(1,'What is the best Star Trek show?',['TNG','Voyager','DS9'],'TNG');
@@ -55,12 +55,13 @@ describe('Card', function() {
     expect(turnB.evaluateGuess()).to.equal(true);
   });
 
-  it.skip('should be able to provide feedback based on the guess', function() {
+  it('should be able to provide feedback based on the guess', function() {
     const card = new Card(1,'What is your favorite food and why is it pizza?',['duh','come on','double duh'],'double duh');
     const turnA = new Turn('duh', card);
     const turnB = new Turn ('double duh', card);
 
-    expect(turn.giveFeedback()).equal(false);
+    expect(turnA.giveFeedback()).equal('Incorrect');
+    expect(turnB.giveFeedback()).equal('Correct');
   });
 
 });
